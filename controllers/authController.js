69f1f86d931ce43a36810ebe06d4221def3be551
NamDo <166409645+Namdo2465@users.noreply.github.com>
2026-04-15 19:19:18 -0500

    Add user to db and check if username or email already existed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,47 @@
+import validator from "validator";
+import { getDBConnection } from "../db/db.js";
+
 export async function registerUser(req, res) {
-  console.log("req.body: ", req.body);
+  let { name, email, username, password } = req.body;
+
+  if (!name || !email || !username || !password) {
+    return res.status(400).json({ error: "All fields are required." });
+  }
+
+  name = name.trim();
+  email = email.trim();
+  username = username.trim();
+
+  if (!/^[a-zA-Z0-9_-]{1,20}$/.test(username)) {
+    return res.status(400).json({
+      error:
+        "Username must be 1–20 characters, using letters, numbers, _ or -.",
+    });
+  }
+
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
+
+  try {
+    const db = await getDBConnection();
+
+    const existing = await db.get(
+      "SELECT id FROM users WHERE username = ? OR email = ?",
+      [username, email]
+    );
+    if (existing) {
+      return res
+        .status(400)
+        .json({ error: "Username or email already exists." });
+    }
+    const result = await db.run(
+      "INSERT INTO users (name, email, username, password) VALUES (?, ?, ?, ?)",
+      [name, email, username, password]
+    );
+    res.status(201).json({ message: "User registered successfully." });
+  } catch (err) {
+    console.error("Registration error:", err.message);
+    res.status(500).json({ error: "Registration failed. Please try again." });
+  }
 }
